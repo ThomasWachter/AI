b3315950c1ae357b25dd59da2d21e61c0ee7a42f
eliquiquenocook <42017241+eliquiquenocook@users.noreply.github.com>
2019-08-22 15:35:36 -0400

    Update foli.py

--- foli.py
+++ foli.py
@@ -1,6 +1,17 @@
 import folium 
 
+#darle locacion al mapa
 map = folium.Map(location=[42.3601, -71.0589],zoom_start=12)
 
-map.save("folium.html")
+#crear un tooltip
+tooltip = "click for more info"
 
+folium.Marker([42.363600, -71.099500],
+                popup="<strong>Location one</strong>",
+                tooltip=tooltip).add_to(map)
+
+
+
+
+#guardar el mapa
+map.save("folium.html")
