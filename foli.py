27d6af4326138ccb954ddaa8c3ced0704ad86769
eliquiquenocook <42017241+eliquiquenocook@users.noreply.github.com>
2019-08-22 15:36:29 -0400

    Update foli.py

--- foli.py
+++ foli.py
@@ -7,8 +7,8 @@ map = folium.Map(location=[42.3601, -71.0589],zoom_start=12)
 tooltip = "click for more info"
 
 folium.Marker([42.363600, -71.099500],
-                popup="<strong>Location one</strong>",
-                tooltip=tooltip).add_to(map)
+                popup="<strong>Location one</strong>", #agrego un popup
+                tooltip=tooltip).add_to(map) #le agrego info extra al popup y debo agregar el "ad_to()"
 
 
 
